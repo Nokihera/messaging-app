d4dd3f1e46818f5d32436f8e215051a381eb74b1
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-08-30 03:19:36 +0630

    Resolved merge conflict in src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SignIn from './pages/signIn'
 import SignUp from './pages/SignUp'
-<<<<<<< HEAD
 import HomePage from './pages/HomePage'
-=======
 import MessageUI from './pages/messageUi'
->>>>>>> dc79a4ca3986a164994855e39f14d3ae3e335f73
 
 const App = () => {
   return (
@@ -14,11 +11,8 @@ const App = () => {
         <Routes>
             <Route path='/' element={<SignIn/>}/>
             <Route path='/signup' element={<SignUp/>}/>
-<<<<<<< HEAD
             <Route path='/home' element={<HomePage/>}/>
-=======
             <Route path='/message' element={<MessageUI/>}/>
->>>>>>> dc79a4ca3986a164994855e39f14d3ae3e335f73
         </Routes>
     </div>
   )
