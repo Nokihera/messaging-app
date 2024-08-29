dc79a4ca3986a164994855e39f14d3ae3e335f73
Nokihera <minzarnihtut811@gmail.com>
2024-08-30 03:19:10 +0700

    nav bar draft added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Content = ({ pf }) => {
   return (
     <div
-      className="flex gap-3 items-center bg-slate-400 border-b-2 border-white py-4 px-6"
+      className="flex gap-3 items-center border-b-2 border-gray-500 py-4 px-6"
     >
       <img
         src={pf.profilePictureUrl}
--- src/pages/messageUi.jsx
+++ src/pages/messageUi.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import UserProfile from '../GlobalState/UseProfile';
-import Content from '../components/Content';
+import React from "react";
+import UserProfile from "../GlobalState/UseProfile";
+import Content from "../components/Content";
+import NavBar from "../components/NavBar";
 
 const MessageUI = () => {
-    const { profile } = UserProfile();
+  const { profile } = UserProfile();
 
-    return (
-        <div className='flex flex-col'>
-            {profile.map((pf, index) => (
-                <Content key={index} pf={pf}/>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col gap-3">
+        <NavBar/>
+      <div className="flex flex-col">
+        {profile.map((pf, index) => (
+          <Content key={index} pf={pf} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MessageUI;
