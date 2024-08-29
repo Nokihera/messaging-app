c82089a58d9002e4bc6cb3e710719f34b07a2f39
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2024-08-30 03:23:36 +0630

    App.jsx is fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import SignIn from './pages/signIn'
-import SignUp from './pages/SignUp'
-<<<<<<< HEAD
-import HomePage from './pages/HomePage'
-=======
-import MessageUI from './pages/messageUi'
->>>>>>> dc79a4ca3986a164994855e39f14d3ae3e335f73
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/signIn";
+import SignUp from "./pages/SignUp";
+import HomePage from "./pages/HomePage";
+import MessageUI from "./pages/messageUi";
 
 const App = () => {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<SignIn/>}/>
-            <Route path='/signup' element={<SignUp/>}/>
-<<<<<<< HEAD
-            <Route path='/home' element={<HomePage/>}/>
-=======
-            <Route path='/message' element={<MessageUI/>}/>
->>>>>>> dc79a4ca3986a164994855e39f14d3ae3e335f73
-        </Routes>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/message" element={<MessageUI />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
