a2bffd0119d38801c157e3fa09aaa844ff8dc450
Nokihera <minzarnihtut811@gmail.com>
2024-08-30 15:24:01 +0700

    navbar completed

--- src/pages/messageUi.jsx
+++ src/pages/messageUi.jsx
@@ -34,7 +34,7 @@ const MessageUI = () => {
 
 
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col">
       <NavBar />
       {currentUser && <div>This is {currentUser.displayName}</div>}
       <div className="flex flex-col">
